0e769c44a9d542f3670b193ff22d78b11e1c8460
hovoaep <hovoaep@gmail.com>
2018-09-04 22:14:07 +0400

    First

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,3 @@
-import './../sass/styles.scss';
-import _ from 'lodash';
-
-const getHeader = () => {
-    const helloWebpack = _.join(_.values({
-        a: 'Hello',
-        b: 'webpack'
-    }), ' ');
-
-    const element = document.createElement('h1');
-
-    element.innerHTML = helloWebpack;
-
-    return element;
-};
-
-document.body.appendChild(getHeader());
+import owlCarusel from "./componetns/OwlCarusel";
+import "../sass/styles.scss";
+owlCarusel;
--- webpack.config.js
+++ webpack.config.js
@@ -1,85 +1,86 @@
-const path = require('path');
-const argv = require('yargs').argv;
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const isDevelopment = argv.mode === 'development';
-const isProduction = !isDevelopment;
-const distPath = path.join(__dirname, '/public');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const distPath = path.join(__dirname, "./public");
 
 const config = {
   entry: {
-    main: './src/js/index.js'
+    main: "./src/js/index.js"
   },
   output: {
-    filename: 'bundle.js',
+    filename: "bundle.js",
     path: distPath
   },
   module: {
-    rules: [{
-      test: /\.html$/,
-      use: 'html-loader'
-    }, {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: [{
-        loader: 'babel-loader',
-        options: {
-          presets: ['env']
-        }
-      }]
-    }, {
-      test: /\.scss$/,
-      exclude: /node_modules/,
-      use: [
-        isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-        {
-          loader: 'css-loader',
+    rules: [
+      {
+        test: /\.html$/,
+        use: "html-loader"
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["env"]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              minimize: true
+            }
+          },
+          "sass-loader",
+          "resolve-url-loader"
+        ]
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "images/[name][hash].[ext]"
+            }
+          },
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 70
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        use: {
+          loader: "file-loader",
           options: {
-            minimize: isProduction
-          }
-        },
-        'sass-loader',
-        'resolve-url-loader'
-      ]
-    }, {
-      test: /\.(gif|png|jpe?g|svg)$/i,
-      use: [{
-        loader: 'file-loader',
-        options: {
-          name: 'images/[name][hash].[ext]'
-        }
-      }, {
-        loader: 'image-webpack-loader',
-        options: {
-          mozjpeg: {
-            progressive: true,
-            quality: 70
+            name: "fonts/[name][hash].[ext]"
           }
         }
-      },
-      ],
-    }, {
-      test: /\.(eot|svg|ttf|woff|woff2)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          name: 'fonts/[name][hash].[ext]'
-        }
-      },
-    }]
+      }
+    ]
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css'
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
+      filename: "[name].css",
+      chunkFilename: "[id].css"
     })
   ],
-  optimization: isProduction ? {
+  optimization: {
     minimizer: [
       new UglifyJsPlugin({
         sourceMap: true,
@@ -89,16 +90,10 @@ const config = {
             warnings: false,
             drop_console: true,
             unsafe: true
-          },
-        },
-      }),
-    ],
-  } : {},
-  devServer: {
-    contentBase: distPath,
-    port: 9000,
-    compress: true,
-    open: true
+          }
+        }
+      })
+    ]
   }
 };
 
